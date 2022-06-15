2bf4b7fe673d5377c2f283d45f1ceba579a30e9d
Kate <kitte246@gmail.com>
2022-06-15 05:14:20 +0300

    В ночь перед защитой

--- API/myvenv/webapp/main/views.py
+++ API/myvenv/webapp/main/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Employees, Offense, Cameras
+import random
+import pickle
+
+def get_offences() -> list:
+    data = [[], []]
+    offenses = Offense.objects.all()
+    for i in range(7):
+        week = offenses.filter(offense_date__week_day=i)
+        data[0].append(week.filter(type_offense='В маске').count())
+        data[1].append(week.exclude(type_offense='В маске').count())
+    return data
+
+def get_offenses_persent() ->list:
+    data = []
+    types = ['В маске', 'Без маски', 'В неправильно надетой маске']
+    offenses = Offense.objects.all()
+    off_count = offenses.count()
+    for t in range(len(types)):
+        element = []
+        element.append(types[t])
+        print(offenses.filter(type_offense=types[t]).count())
+        element.append(offenses.filter(type_offense=types[t]).count() / off_count * 100)
+        data.append(element)
+    print(data)
+    return data
 
 def index(request):
-    offenses = Offense.objects.all()
     top_employees = Employees.objects.order_by('number_of_offenses')[:10]
-    
+    index = random.randint(0, 3)
+    videos = [
+        "static/webapp/videos/Baiden.mp4",
+        "static/webapp/videos/Trump.mp4",
+        "static/webapp/videos/Putin.mp4",
+        "static/webapp/videos/Putin2.mp4"
+        ]
+    video = videos[index]
 
-    return render(request, 'main.html', {'top_employees':top_employees})
+    data = get_offences()
+    data_persent = get_offenses_persent
+
+    return render(request, 'main.html', {'top_employees':top_employees, 'video':video, 'data':data, 'data_persent': data_persent})
--- API/myvenv/webapp/statistic/views.py
+++ API/myvenv/webapp/statistic/views.py
@@ -1,4 +1,46 @@
 from django.shortcuts import render
+from main.models import Employees, Offense, Cameras
+
+def get_offences_for_cam() -> list:
+    data = [[], [], []]
+    types = ['В маске', 'Без маски', 'В неправильно надетой маске']
+    offenses = Offense.objects.all()
+    cam_ids = Cameras.objects.order_by().values('id_camera').distinct()
+    for t in range(len(types)):
+        offenses_by_type = offenses.filter(type_offense=types[t])
+
+        for id in cam_ids:
+            data[t].append(offenses_by_type.filter(id_camera=id['id_camera']).count())
+    return data
+
+def get_offences_for_department() -> list:
+    data = [[], [], []]
+    offenses = Offense.objects.all()
+    departments = Employees.objects.order_by().values('department').distinct()
+    types = ['В маске', 'Без маски', 'В неправильно надетой маске']
+    for t in range(len(types)):
+        offenses_by_type = offenses.filter(type_offense=types[t])
+
+        for department in departments:
+            data[t].append(offenses_by_type.filter(id_employee__department=department['department']).count())
+
+    return data
+
+def get_offenses_for_month() -> list:
+    data = [[], [], []]
+    offenses = Offense.objects.all()
+    for i in range(1, 13):
+        week = offenses.filter(offense_date__month=i)
+        data[0].append(week.filter(type_offense='В маске').count())
+        data[1].append(week.filter(type_offense='Без маски').count())
+        data[2].append(week.filter(type_offense='В неправильно надетой маске').count())
+    print(data)
+    return data
 
 def statistic_home(request):
-    return render(request, 'statistic.html')
+
+    cam = get_offences_for_cam()
+    dep = get_offences_for_department()
+    month = get_offenses_for_month()
+
+    return render(request, 'statistic.html', {'cam_offenses':cam, 'dep_offenses':dep, 'month':month})
